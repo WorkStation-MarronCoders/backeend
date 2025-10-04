4f71ce8923f5c5e743c72a2438fbb66d256bd684
Rodrigo Liberato <129563169+Libeman10@users.noreply.github.com>
2025-10-04 16:52:40 -0500

    chore: System Test achieved

--- obj/Debug/net9.0/workstation-backend.AssemblyInfo.cs
+++ obj/Debug/net9.0/workstation-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("workstation-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cc76f8892d12c30e9d2c5cd802ba27d4387795c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8544105dc9593666622c9dd46dcf31659d6ac18d")]
 [assembly: System.Reflection.AssemblyProductAttribute("workstation-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("workstation-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
